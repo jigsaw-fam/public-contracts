1e717caffab933f9cb305bed013cf3aedb6aef6d
diewland <diewland@gmail.com>
2023-10-21 00:25:01 +0700

    add claim-limit

--- BoredTownTokenDistributor.sol
+++ BoredTownTokenDistributor.sol
@@ -4,25 +4,23 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-// 1. setToken
-// 2. setRecipients
-// 3. toggleClaim
 contract BoredTownTokenDistributor is Ownable {
+
+    IERC20 public token;
+    mapping(address => uint256) public claimableTokens;
+    uint256 public totalClaimable = 0;
+    bool public claimEnabled = false;
+    uint256 public claimLimit = 12_500000000000000000; // 12.5
 
     // events
     event CanClaim(address indexed recipient, uint256 amount);
     event HasClaimed(address indexed recipient, uint256 amount);
     event Withdrawal(address indexed recipient, uint256 amount);
 
-    // token
-    IERC20 public token;
+    // owner functions
     function setToken(address newToken) external onlyOwner {
         token = IERC20(newToken);
     }
-
-    // update recipients
-    mapping(address => uint256) public claimableTokens;
-    uint256 public totalClaimable;
     function setRecipients(address[] calldata _recipients, uint256[] calldata _claimableAmount)
         external
         onlyOwner
@@ -43,17 +41,15 @@ contract BoredTownTokenDistributor is Ownable {
     function resetTotalClaimable() external onlyOwner {
         totalClaimable = 0;
     }
-
-    // owner withdraw
     function withdraw(uint256 amount) public onlyOwner {
         require(token.transfer(msg.sender, amount), "fail transfer token");
         emit Withdrawal(msg.sender, amount);
     }
-
-    // toggle round
-    bool public claimEnabled = false;
     function toggleClaim() external onlyOwner {
         claimEnabled = !claimEnabled;
+    }
+    function setClaimLimit(uint256 newLimit) external onlyOwner {
+        claimLimit = newLimit;
     }
 
     // claim token
@@ -62,7 +58,8 @@ contract BoredTownTokenDistributor is Ownable {
 
         uint256 amount = claimableTokens[msg.sender];
         require(amount > 0, "nothing to claim");
-        require(token.balanceOf(address(this)) >= amount, "not enough token");
+        require(amount <= claimLimit, "over claim limit");
+        require(amount <= token.balanceOf(address(this)), "not enough token");
 
         claimableTokens[msg.sender] = 0;
 
