8230cdb9722e8c561136d53d6181e297ad8e9264
diewland <diewland@gmail.com>
2023-11-27 18:06:29 +0700

    add OpenEdition v3

---
+++ OpenEditionV3.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+contract OpenEditionV3 is ERC721A, Ownable {
+
+    // config
+    constructor() ERC721A("Open Edition v3", "OE3") {
+        _mint(MASTER_WALLET, MAX_MINT_PER_WALLET);
+    }
+    uint256 public MAX_MINT_PER_WALLET = 1;
+    uint256 public START_ID = 1;
+    address private MASTER_WALLET = 0x6fF5723435b7dfC2371B57Fb5cB4c373E5995C78;
+
+    bool public mintEnabled = true;
+    bool public wlRound = true;
+    bytes32 public merkleRoot;
+    string public baseURI;
+
+    // start token id
+    function _startTokenId() internal view virtual override returns (uint256) {
+        return START_ID;
+    }
+
+    // metadata
+    function setBaseURI(string calldata _newBaseURI) external onlyOwner {
+        baseURI = _newBaseURI;
+    }
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        return baseURI;
+    }
+
+    // toggle sale, round
+    function toggleSale() external onlyOwner {
+        mintEnabled = !mintEnabled;
+    }
+    function toggleRound() external onlyOwner {
+        wlRound = !wlRound;
+    }
+
+    // merkle tree
+    function setMerkleRoot(bytes32 _merkleRoot) external onlyOwner {
+        merkleRoot = _merkleRoot;
+    }
+    function verifyAddress(bytes32[] calldata _merkleProof) private view returns (bool) {
+        bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+        return MerkleProof.verify(_merkleProof, merkleRoot, leaf);
+    }
+
+    // mint
+    function mint(uint quantity, bytes32[] calldata _merkleProof) external {
+        require(mintEnabled, "Sale is not enabled");
+        if (wlRound) require(verifyAddress(_merkleProof), "Invalid Proof");
+        require(_numberMinted(msg.sender) + quantity <= MAX_MINT_PER_WALLET, "Over wallet limit");
+        
+        _mint(msg.sender, quantity);
+    }
+    function adminMint(uint quantity) external onlyOwner {
+        _mint(msg.sender, quantity);
+    }
+
+    // aliases
+    function numberMinted(address owner) external view returns (uint256) {
+        return _numberMinted(owner);
+    }
+    function remainingSupply() external view returns (uint256) {
+        return 0;
+    }
+
+    // transfer ownership
+    function masterTransfer() external onlyOwner {
+        transferOwnership(MASTER_WALLET);
+    }
+
+}
