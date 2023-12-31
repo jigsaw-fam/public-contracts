9b1c4c68d3b8711c2395912b0cf716d70b59e00a
diewland <diewland@gmail.com>
2023-12-31 18:51:50 +0700

    copy v3 to zkfair, prepare to custom

---
+++ jkr-launchpad/zkfair/config.js
@@ -0,0 +1,18 @@
+// chain
+const CHAIN_NAME      = "ZKFair Mainnet";
+const CHAIN_RPC       = "https://rpc.zkfair.io";
+const CHAIN_ID        = 42766;
+const CHAIN_SYMBOL    = "USDC";
+const CHAIN_EXPLORER  = "https://scan.zkfair.io";
+
+// contract
+const MAX_SUPPLY      = 100;
+const MINT_PER_WALLET = 1;
+const CONTRACT_ADDR   = "0xc14F462E63f36D9bE62b2Fbb7595E844d3bc18Ee";
+const MINT_GAS_RATE   = 1;
+
+// whitelist
+const PROOF_URL       = null;
+
+// twitter
+const TWEET_TEXT      = null;
