7e202ed71dd4628eaab2ebb015d8183eee9a06a7
diewland <diewland@gmail.com>
2024-01-15 09:11:51 +0700

    prepare bera mint

---
+++ jkr-launchpad/berachain_testnet/config.js
@@ -0,0 +1,25 @@
+// chain
+/* *** TODO ****
+const CHAIN_NAME      = "Berachain Artio";
+const CHAIN_RPC       = "https://artio.rpc.berachain.com/";
+const CHAIN_ID        = 80085;
+const CHAIN_SYMBOL    = "BERA";
+const CHAIN_EXPLORER  = "https://artio.beratrail.io/";
+*/
+const CHAIN_NAME      = "Sepolia Testnet";
+const CHAIN_RPC       = "https://eth-sepolia.g.alchemy.com/v2/ImOC9kCyGPgZDj0dEAAyTc9v5_tGO1Z7";
+const CHAIN_ID        = 11155111;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://sepolia.etherscan.io";
+
+// contract *** TODO ***
+const MAX_SUPPLY      = 100;
+const MINT_PER_WALLET = 1;
+const CONTRACT_ADDR   = "0x6c0D21BDBeD4911076a6a494B97dFe69DdF1Fd7c";
+const MINT_GAS_RATE   = 1;
+
+// whitelist
+const PROOF_URL       = null;
+
+// twitter
+const TWEET_TEXT      = null;
