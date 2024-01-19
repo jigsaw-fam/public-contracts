7d86ba37f636f890a3f53b3425903934c3dbb846
diewland <diewland@gmail.com>
2024-01-19 23:21:14 +0700

    add BeraGirl

---
+++ jkr-launchpad/beragirl_testnet/config.js
@@ -0,0 +1,18 @@
+// chain
+const CHAIN_NAME      = "Berachain Artio";
+const CHAIN_RPC       = "https://artio.rpc.berachain.com/";
+const CHAIN_ID        = 80085;
+const CHAIN_SYMBOL    = "BERA";
+const CHAIN_EXPLORER  = "https://artio.beratrail.io/";
+
+// contract
+const MAX_SUPPLY      = 10_000;
+const MINT_PER_WALLET = 10;
+const CONTRACT_ADDR   = "0xc14F462E63f36D9bE62b2Fbb7595E844d3bc18Ee";
+const MINT_GAS_RATE   = 1;
+
+// whitelist
+const PROOF_URL       = null;
+
+// twitter
+const TWEET_TEXT      = null;
